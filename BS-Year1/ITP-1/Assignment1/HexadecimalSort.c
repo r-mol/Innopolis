cb3f8fb40a70e16f4e4dae7cb68efaed1bb66e87
Roman <molochkov2017@gmail.com>
2021-09-26 08:15:04 +0300

    Write program which register tenants in ski resort data base

---
+++ BS-Year1/ITP-1/Assignment1/HexadecimalSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fin = fopen("input.txt","rb");
+    FILE *fout = fopen("output.txt","w");
+
+    int arr[1000];
+    int i = 0;
+
+    //Input
+    while (!feof(fin))
+    {
+        fscanf(fin, "%X", &arr[i]);
+        i++;
+    }
+
+    int n = i;
+
+    for(i = 0;i < n - 1; i++){
+        for(int j = i +1; j < n; j++){
+            if(arr[i]> arr[j]){
+                int temp = arr[i];
+                arr[i]= arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n;i++) {
+        fprintf(fout,"%X ",arr[i]);
+
+    }
+
+    fclose(fin);
+    fclose(fout);
+    return 0;
+}
