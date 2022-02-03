ea115d30e4ef9d1fca3de491a7777b35c45a6efe
Roman <molochkov2017@gmail.com>
2022-02-03 17:46:53 +0300

    add : LinkedHashMap.h & linkedhashmap.h

--- BS-Year1/DSA/Lab-3/FrequencyAnalysis.cpp
+++ BS-Year1/DSA/Lab-3/FrequencyAnalysis.cpp
@@ -4,46 +4,57 @@
 #include <vector>
 #include <bits/stdc++.h>
 
-bool cmp(std::pair<std::string, int>& a,std::pair<std::string, int>& b)
-{
-    return a.second < b.second;
+bool cmp(std::pair<std::string, int> &a, std::pair<std::string, int> &b) {
+    if (a.second == b.second) {
+        return a.first > b.first;
+    } else {
+        return a.second < b.second;
+    }
 }
 
-void sort(std::map<std::string, int>& M)
-{
+void sortAndPrint(std::map<std::string, int> &M) {
     std::vector<std::pair<std::string, int> > A;
 
-    for (auto& it : M) {
+    A.reserve(M.size());
+    for (auto &it: M) {
         A.emplace_back(it);
     }
 
     sort(A.begin(), A.end(), cmp);
-    reverse(A.begin(), A.end() );
-    for (auto& it : A) {
-
+    reverse(A.begin(), A.end());
+    for (auto &it: A) {
         std::cout << it.first << ' ' << it.second << std::endl;
     }
 }
 
-int main(){
-    std::string str;
-    getline(std::cin, str);
-    std::map<std::string, int> map1;
+void fillTheMap(std::map<std::string, int> &map1, std::string str) {
     std::string tempStr;
-
-    for(int i = 0; i <= str.length(); i++){
-        if(str[i]!=' '&& str[i]!= '\0'){
-            tempStr+= str[i];
-        }
-        else{
+    for (int i = 0; i <= str.length(); i++) {
+        if (str[i] != ' ' && str[i] != '\0') {
+            tempStr += str[i];
+        } else {
             auto search = map1.find(tempStr);
             if (search != map1.end()) {
                 search->second++;
             } else {
-                map1.insert(std::pair<std::string, int>(tempStr,1));
+                map1.insert(std::pair<std::string, int>(tempStr, 1));
             }
             tempStr = "";
         }
     }
-    sort(map1);
+}
+
+void frequencyAnalysis() {
+    std::string num;
+    std::string str;
+    getline(std::cin, num);
+    getline(std::cin, str);
+    std::map<std::string, int> map1;
+
+    fillTheMap(map1, str);
+    sortAndPrint(map1);
+}
+
+int main() {
+    frequencyAnalysis();
 }
